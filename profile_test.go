4c88d00bf8946a5e6f7ae33aafc0ea925fcf2295
Dave Cheney <dave@cheney.net>
2013-05-22 20:26:47 +1000

    removed useless example

--- profile_test.go
+++ profile_test.go
@@ -11,9 +11,3 @@ func ExampleStart() {
 	// profiling data to disk.
 	defer prof.Stop()
 }
-
-func ExampleStop() {
-	// start a default profile and cleanly
-	// flush the data to disk on exit.
-	defer profile.Start(nil).Stop()
-}
