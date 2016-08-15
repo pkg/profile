f1c84024fdf50bbf4f2a6f3624e844603fa10def
Dave Cheney <dave@cheney.net>
2016-08-15 13:14:29 +1000

    Rename private profile type to Profile

--- trace.go
+++ trace.go
@@ -5,7 +5,7 @@ package profile
 import "runtime/trace"
 
 // Trace profile controls if execution tracing will be enabled. It disables any previous profiling settings.
-func TraceProfile(p *profile) { p.mode = traceMode }
+func TraceProfile(p *Profile) { p.mode = traceMode }
 
 var startTrace = trace.Start
 var stopTrace = trace.Stop
